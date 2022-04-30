d4dcc3fe422f96e39c9eba2e3b749efa8b5f9c4d
Mars Balisacan <mars.brwallc@gmail.com>
2022-04-30 14:15:44 -0700

    snow shoveler script

---
+++ Unity - TeamOneJamGame/Assets/Scripts/SnowShoveler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowShoveler : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject snowBlock;
+	[SerializeField]
+	private bool hasSnow;
+    
+	private void OnTriggerEnter(Collider other)
+	{
+		if ( other.tag == "Snow")
+		{
+			snowBlock = other.gameObject;
+		}
+	}
+}
